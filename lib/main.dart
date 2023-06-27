b56bf812cd52b93d3c6c2d53029fbe3f59964fc8
David Felix <davidffelix@hotmail.com>
2023-06-27 10:27:26 -0300

    feat: added Container

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.green,
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,23 +7,63 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('AppBar'),
+        title: const Text('AppBar'),
       ),
-      body: Container(
-        height: 200,
-        width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-          color: Colors.blue,
-        ),
-        child: const Center(
-          child: Text(
-            'Olá Mundo!',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: const Center(
+              child: Text(
+                'Olá Mundo!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
             ),
           ),
-        ),
+          Row(
+            children: [
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: 200,
+                child: const Center(
+                  child: Text('Container 1'),
+                ),
+              ),
+              Container(
+                color: Colors.purple,
+                height: 100,
+                width: 192.5,
+                child: const Center(
+                  child: Text('Container 2'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.ac_unit),
+            label: 'Item 1',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_alarm),
+            label: 'Item 2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
+            label: 'Item 3',
+          ),
+        ],
       ),
     );
   }
